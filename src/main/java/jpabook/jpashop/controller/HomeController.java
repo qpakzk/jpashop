454a85b5f474d5ec133d64d501037c2bb96c0d14
Sangwon Hong <qpakzk@gmail.com>
2020-12-01 02:17:10 +0900

    delete unused packages

--- src/main/java/jpabook/jpashop/controller/HomeController.java
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -1,8 +1,6 @@
 package jpabook.jpashop.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
